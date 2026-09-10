4ee91599c6089535b04d0f51e4a4a24fc3fb5921
Dušan Mihaljica <mihaljica.dusan@gmail.com>
2026-09-10 15:53:00 +0200

    Premestene SQL skripte i dodata tabela korisnika

--- SistemPutovanja.Podaci/BazaPodataka/StrukturaBazePodataka.sql
+++ SistemPutovanja.Podaci/BazaPodataka/StrukturaBazePodataka.sql
@@ -31,6 +31,20 @@ CREATE TABLE StavkaZahteva (
     CONSTRAINT FK_Stavka_Zahtev FOREIGN KEY (BrojZahteva) REFERENCES ZahteviZaPutovanje(BrojZahteva) ON DELETE CASCADE
 );
 
+CREATE TABLE Korisnici (
+    KorisnikID INT PRIMARY KEY IDENTITY(1,1),
+    KorisnickoIme NVARCHAR(50) NOT NULL,
+    Lozinka NVARCHAR(100) NOT NULL,
+    Uloga NVARCHAR(30) NOT NULL, -- Npr. 'Direktor', 'Admin', 'Zaposleni'
+    ZaposleniId INT NULL,        -- Opciono vezivanje za tabelu Zaposleni
+    CONSTRAINT FK_Korisnici_Zaposleni FOREIGN KEY (ZaposleniId) REFERENCES Zaposleni(ZaposleniId)
+);
+
 INSERT INTO Zaposleni (Ime, Prezime, Pozicija, Email) VALUES 
 ('Marko', 'Marković', 'Developer', 'marko@test.com'),
 ('Nikola', 'Nikolić', 'Menadžer', 'nikola@test.com');
+
+INSERT INTO Korisnici (KorisnickoIme, Lozinka, Uloga, ZaposleniId) VALUES 
+('admin', 'admin123', 'Administrator', NULL),
+('direktor_marko', 'lozinka123', 'Direktor', 1),
+('menadzer_nikola', 'lozinka123', 'Direktor', 2);
